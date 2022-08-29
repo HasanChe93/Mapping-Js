ccb366123b0a0a044fa20b789724428d7bc69223
HasanAbuHashem <hasan.aabuhashem@gmail.com>
2022-08-29 16:47:06 +0300

    Mapping

--- Script/Mapping.js
+++ Script/Mapping.js
@@ -1,6 +1,5 @@
 Instructions: 
- - You should solve all these questions using .map() function 
-*/
+//  - You should solve all these questions using .map() function 
 
 
 /*
@@ -15,6 +14,10 @@ Instructions:
  * Result:
  * [4, 10, 200]
 */
+let numbers=[2,40,80,500]
+map1=numbers.map(n=>n*2) 
+console.log(map1)
+
 //  * Exercise 2 : 
 //  * 
 //  * Take an array of numbers and make them strings
@@ -70,6 +73,32 @@ Instructions:
  * Result:
  *  ["Angelina Jolie", "Eric Jones", "Paris Hilton", "Kayne West", "Bob Ziroll"]
 */
+ function namesOnly(arr) {
+    return arr.map(i => i.name)
+  }
+  var res = namesOnly([
+    {
+      name: "Angelina Jolie",
+      age: 80
+    },
+    {
+      name: "Eric Jones",
+      age: 2
+    },
+    {
+      name: "Paris Hilton",
+      age: 5
+    },
+    {
+      name: "Kayne West",
+      age: 16
+    },
+    {
+      name: "Bob Ziroll",
+      age: 100
+    }
+  ])
+  console.log(res);
 /*
  * Exercise 5:  
  * 
@@ -109,6 +138,43 @@ Instructions:
  * "Bob Ziroll can go to The Matrix"]
  * 
  */
+function makeStrings (arr) { 
+    let strings = arr.map(function (string) {
+  
+    if (string.age >= 18) {
+    
+    return string.name + 'can go to The Matrix';
+    
+    }
+     else {
+    return string.name + 'under age!';
+    }})
+  return strings ;
+  }
+  
+  
+    console.log(makeStrings([
+     {
+        name: "Angelina Jolie",
+        age: 80
+    },
+    {
+        name: "Eric Jones",
+        age: 2
+    },
+    {
+        name: "Paris Hilton",
+        age: 5
+    },
+    {
+        name: "Kayne West",
+        age: 16
+    },
+    {
+        name: "Bob Ziroll",
+        age: 100
+    }
+  ]))
 // * Exercise 6:  
 //  * 
 //  * Make an array of the names in h1s, and the ages in h2s
@@ -139,6 +205,40 @@ Instructions:
 //  * ])); 
 //  * 
 //  * Result:
+function readyToPutInTheDOM(arr) {
+    arr.map(c=> {
+       document.write("<h1>" + c.name + "</h1>");
+       document.write("<h2>" + c.age+ "</h2>");
+    });
+
+    arr.forEach(c=> {
+       document.write("<h1>" + c.name + "</h1>");
+       document.write("<h2>" + c.age+ "</h2>");
+    });
+  }
+
+  readyToPutInTheDOM([
+    {
+      name: "Angelina Jolie",
+      age: 80
+    },
+    {
+      name: "Eric Jones",
+      age: 2
+    },
+    {
+      name: "Paris Hilton",
+      age: 5
+    },
+    {
+      name: "Kayne West",
+      age: 16
+    },
+    {
+      name: "Bob Ziroll",
+      age: 100
+    }
+  ])
 //  * 
 //  * ["<h1>Angelina Jolie</h1><h2>80</h2>", 
 //  * "<h1>Eric Jones</h1><h2>2</h2>", 
@@ -157,6 +257,11 @@ Instructions:
 // * Test Case 1: [2,4,6] 
 // * Test Case 2: [2,-4,-6]
 // */
+function doubleValues(arr){
+
+    console.log(arr.map(c => c*2))
+    }
+    doubleValues([2,3,8])
 //   * Exercise 8:
 //   * Write a function called valTimesIndex which accepts an array and returns a new array with each value multiplied by the index it is currently at in the array.
 //   *
@@ -168,6 +273,10 @@ Instructions:
 //   * Result
 //   * Test Case 1: [0,2,6]
 //   * Test Case 2: [0,-2,-6]
+
+var array = [1, 2, 3]
+var result = array.map((x,i) => x*i);
+console.log(result);
 //   //
 //   * Exercise 9:
 //   * Write a function called extractKey which accepts an array of objects and some key and returns a new array with the value
@@ -179,6 +288,16 @@ Instructions:
 //   * Result:
 //   * ['Elie', 'Tim', 'Matt', 'Colt']
 //   //
+function extractKey(arr, key) {
+    return arr.map(a => a[key]);
+  }
+  
+  console.log(
+    extractKey(
+      [{ name: "Elie" }, { name: "Tim" }, { name: "Matt" }, { name: "Colt" }],
+      "name"
+    )
+  );
 //   * Exercise 10:
 //   * Write a function called extractFullName which accepts an array of objects and returns a new array with the value of the key with a name of "first" and the value of a key with the name of  "last" in each object, concatenated together with a space. 
 //   * 
@@ -186,6 +305,10 @@ Instructions:
 //   * extractFullName([{first: 'Elie', last:"Schoppik"}, {first: 'Tim', last:"Garcia"}, {first: 'Matt', last:"Lane"}, {first: 'Colt', last:"Steele"}]) // ['Elie Schoppik', 'Tim Garcia', 'Matt Lane', 'Colt Steele']
 // //
 //Filter 10 - 15 you should use Array.prototype.filter
+function extractFullName(namesarray){
+    return namesarray.map(c =>`${c.first} ${c.last}`)} 
+  
+  console.log(extractFullName([{first: 'Elie', last:"Schoppik"}, {first: 'Tim', last:"Garcia"}, {first: 'Matt', last:"Lane"}, {first: 'Colt', last:"Steele"}]) )
 /*
 10
 Create a function called evenOnly
@@ -205,6 +328,10 @@ and return an array of these number that is a mutiply by 4
 Ex: multiFour([1,8,6,4]) => [8,4]
 */
 
+var multiplyFour = [11,2,6,8,4,9,5,12,]
+var filter1=multiplyFour.filter(n => n%4===0)
+console.log(filter1)
+
 
 /*
 12
@@ -217,7 +344,14 @@ and return an array of these string that contain this char
 Ex: containChar(["hello","world"],w) => ["world"]
 Ex: containChar(["hello","world"],l) => ["hello","world"]
 */
-
+function containChar(arr, str) {
+    return arr.filter(a => {
+      if (a[0] || a[index]  == str) {
+        return a;
+      }
+    });
+  }
+  console.log(containChar(["hello", "world","wing","web","rewind"], "w"));
 
 /*
 13
@@ -228,7 +362,16 @@ and return an array that have the string with odd length in even index
 var strings= ["alex","mercer","madrasa","rashed2","emad","hala"]
 Ex: evenIndexOddLength(strings) => ["madrasa"]
 */
-
+function evenIndexOddLength(arr) {
+    return arr.filter((n, i) => {
+      if (n.length % 2 == 1 && i % 2 == 0) {
+        return n;
+      }
+    });
+  }
+  var strings = ["alex", "mercer", "madrasa", "rashed2", "emad", "hala"]
+  
+   console.log(evenIndexOddLength(strings))
 
 /*
 14
@@ -239,6 +382,11 @@ and return the person that have age older than this number
 
 Ex: olderThan(persons,56) => [{ name: { first: 'Soso', last: 'Al-Amora' }, age: 67 }]
 */
+function olderThan(arr, age) {
+    return arr.filter(a =>a.age > age) 
+      
+      }
+  console.log(olderThan([{ name: { first: 'Soso', last: 'Al-Amora' }, age: 67 }], 56));
 
 /*
 15
@@ -249,3 +397,12 @@ and return the shorter string than the number
 var strings= ["alex","mercer","madrasa","rashed2","emad","hala"]
 Ex: shorterThan(strings,5) => ["alex","emad","hala"]
 */
+
+function shorterThan(arr, num) {
+    return arr.filter(a => {
+      if (a.length < num) {
+        return a;
+      }
+    });
+  }
+  console.log(shorterThan(['this','that','them','there','it','how','or'],3))
